998cdb42e762c06c1ea18c4202717d3311750424
vs-kimhy <kimhy9@visang.com>
2021-07-23 22:09:07 +0900

    .

---
+++ springboot-admin/src/main/java/me/App.java
@@ -0,0 +1,15 @@
+package me;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@EnableAdminServer
+public class App {
+
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+
+}
